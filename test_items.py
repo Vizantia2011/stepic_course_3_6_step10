e14516c0b9153ade7782057c710a0ab2686a841e
Viantia2011 <marina.a.polhanova@gmail.com>
2023-05-30 17:40:24 +0300

    Урок 3.6 шаг 10

---
+++ test_items.py
@@ -0,0 +1,12 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import math
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+def test_guest_should_see_login_link(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
